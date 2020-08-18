9e6adb98bb2eb826da2bbfb4b4c05ae7d7f148d9
Richard Kurwitz <kurwitz3@aol.com>
2020-08-18 23:24:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require 'pry'
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+def batch_badge_creator(array)
+new_array = []
+array.map do |x|
+new_array << "Hello, my name is #{x}."
+end
+new_array 
+end
+def assign_rooms(speakers)
+ rooms = []
+speakers.each_with_index do |person,number|
+   rooms << "Hello, #{person}! You'll be assigned to room #{number + 1}!"
+ end
+ rooms
+end
+def printer()
+ a = batch_badge_creator
+ a.cycle(1){|x| puts x } 
+b = assign_rooms
+b.cycle(1){|b| puts b }
+end 
+
+
